ea942f6693f2bdd319e23237d95505fcd3a50529
Prasun Nandy Pritam <pritamnandy84@yahoo.com>
2022-11-30 01:03:20 +0600

    30th Nov

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router';
 export default function Profile() {
   const navigate = useNavigate()
   const auth = getAuth();
+  const [changeDetail, setChangeDetail] = useState(false);
   const [ formData, setFormData ] = useState({
     name: "Prasun",
     email: "test@gmail.com"
@@ -14,6 +15,15 @@ export default function Profile() {
     auth.signOut();
     navigate('/')
   }
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }))
+  }
+  function onSubmit() {
+
+  }
   return (
     <>
       <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -21,8 +31,10 @@ export default function Profile() {
           <div className='w-full md:w-[50%] mt-5'>
             <form>
               <input type="text" placeholder='Enter Name' id='name' value={name}
-              className='w-full px-4 py-2 text-xl rounded bg-white border-gray-300 transition ease-in-out text-gray-700 mb-5'
-              disabled
+              className={`w-full px-4 py-2 text-xl rounded bg-white border-gray-300 
+              transition ease-in-out text-gray-700 mb-5 ${changeDetail && "bg-red-300"} focus:bg-white`}
+              disabled={!changeDetail}
+              onChange={onChange}
               />
 
             <input type="email" placeholder='Enter Email' id='email' value={email}
@@ -34,8 +46,9 @@ export default function Profile() {
                 <p>Do you want to change your name?
                   <span 
                   className='text-red-600 hover:text-red-700 active:text-red-800 transition duration-200 ease-in-out
-                  ml-1 cursor-pointer font-semibold'>
-                    Edit</span>
+                  ml-1 cursor-pointer font-semibold'
+                  onClick={() => setChangeDetail((prevState) => !prevState)}>
+                    {changeDetail ? 'Apply Change' : 'Edit'}</span>
                 </p>
                 <p className='text-blue-600 hover:text-blue-700 active:text-blue-800 transition duration-200 ease-in-out cursor-pointer font-semibold' onClick={onLogout}>Sign out</p>
               </div>
